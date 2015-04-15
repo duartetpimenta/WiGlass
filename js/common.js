584b86758f18adcd491685b3047f51f9b5b58cd1
Duarte Pimenta <nine_lives_net@hotmail.com>
2015-04-15 18:22:41 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Duarte on 15-04-2015.
+ */
+function common(){
+    this.about = 'Acerca';
+    this.contacts = 'Contactos';
+    this.gallery = 'Galeria';
+    this.home = 'Início';
+}
+
+common.prototype.test = function(){
+    alert('oi');
+}
